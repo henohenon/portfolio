b3d84b9ef7feda851adbf26f11b26482d16cde58
henohenon <takeru.hawk.son@gmail.com>
2023-12-11 12:14:00 +0900

    ブログのリスト表示の追加

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// パラメータに基づいてリンクのリストを自動生成
+var blogList = document.getElementById('blog-list');
+var mdSettings = {}; // mdSettings.jsonから取得したデータ
+fetch('/blogs/mdSettings.json')
+    .then(response => response.json())
+    .then(data => {
+        mdSettings = data;
+        for (var key in mdSettings) {
+            var listItem = document.createElement('li');
+            var link = document.createElement('a');
+            link.href = 'blogs/?blog=' + key;
+            link.textContent = mdSettings[key].title;
+            listItem.appendChild(link);
+            blogList.appendChild(listItem);
+        }
+    });
